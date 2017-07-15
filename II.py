804fa098f59357abe6cd010caf3ce3511565cc55
vineethk57 <vineethkrishnan096@gmail.com>
2017-07-15 15:02:17 +0530

    Update II.py

--- II.py
+++ II.py
@@ -1,5 +1,5 @@
-num = int(input("Enter a number: "))
+num = 50
 if (num % 2) == 0:
-   print("{0} is Even".format(num))
+   print("Number is Even")
 else:
-   print("{0} is Odd".format(num))
+   print("Number is Odd")
